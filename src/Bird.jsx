30e25a26904a9d0298ca27043e7471812261b6da
JATIN-RAGHAV <jrag137@gmail.com>
2024-11-10 22:51:20 +0530

    small gift for America

--- src/Bird.jsx
+++ src/Bird.jsx
@@ -7,16 +7,18 @@ export default function Bird({ y, size }) {
       <div
         style={{
           transformOrigin: 'top right',
-          transform: `translateY(${y - 100}px) translateX(${distFromLeft}px)`,
-          width: size,
-          height: size,
+          transform: `translateY(${y - 100}px) translateX(${distFromLeft}px) scaleX(-1)`,
           transition: 'transform 0.01s ease',
           position: 'fixed',
           zIndex: '2',
-          backgroundColor: 'red',
           borderRadius: '100%'
         }}
       >
+        <img
+          width={size}
+          height={size}
+          src="https://www.svgrepo.com/show/493481/plane.svg"
+        />
       </div>
     </>
   )
